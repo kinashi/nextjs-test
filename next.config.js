16f0f59feb7e771db0a383c68c350c0db68a7447
kinashi <y.nashiki@gmail.com>
2022-10-22 22:25:27 +0900

    Add styled-components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
